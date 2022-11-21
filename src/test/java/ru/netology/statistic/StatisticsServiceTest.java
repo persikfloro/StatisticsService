b47a1fa2c9e9facb0c2bdbbbbf4f64a7f47a6be4
Надя Трансвалева <liberavivi888@gmail.com>
2022-11-21 15:07:21 +0300

    Тесты и 100% покрытие

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,11 +10,35 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomes);
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIf() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomes);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxForOne() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+        long expected = 2;
+
+        long actual = service.findMax(incomes);
+
+        assertEquals(expected, actual);
+    }
+
+
 }
